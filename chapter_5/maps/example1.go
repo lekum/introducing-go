04aff8ef3aaea7451857c5b14b35fbb7d2570117
Alejandro Guirao <lekumberri@gmail.com>
2016-02-11 08:46:32 +0100

    Add examples for maps and funcs

---
+++ chapter_5/maps/example1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+
+	fmt.Println(elements["He"])
+
+	if name, ok := elements["foo"]; ok {
+		fmt.Println(name)
+	}
+
+	if name, ok := elements["H"]; ok {
+		fmt.Println(name)
+	}
+
+	elements = map[string]string{
+		"H":  "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B":  "Boron",
+		"C":  "Carbon",
+		"N":  "Nitrogen",
+		"O":  "Oxygen",
+		"F":  "Fluorine",
+		"Ne": "Neon",
+	}
+}
---
+++ chapter_6/functions/example1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func average(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
+}
+
+func main() {
+	x := []float64{4, 52, 6}
+	fmt.Println(average(x))
+}
