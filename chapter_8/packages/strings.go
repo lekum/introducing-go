4f9a53dddb7a981f1b6a48a7b4920fc8c3782b1d
Alejandro Guirao <lekumberri@gmail.com>
2016-02-16 07:31:26 +0100

    Add examples of str

--- chapter_8/packages/strings.go
+++ chapter_8/packages/strings.go
@@ -12,4 +12,12 @@ func main() {
 	fmt.Println(strings.HasSuffix("test", "est"))
 	fmt.Println(strings.Index("test", "s"))
 	fmt.Println(strings.Join([]string{"a", "b"}, "-"))
+	fmt.Println(strings.Repeat("a", 5))
+	fmt.Println(strings.Replace("aaaa", "a", "b", 2))
+	fmt.Println(strings.Split("a-b-c-d-e", "-"))
+	fmt.Println(strings.ToUpper("acd"))
+	arr := []byte("test")
+	fmt.Println(arr)
+	str := string([]byte{'t', 'e', 's', 't'})
+	fmt.Println(str)
 }
