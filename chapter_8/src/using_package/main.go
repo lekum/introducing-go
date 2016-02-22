4bf0b551c40c5dd440134de4f5dadbfb61ea0409
Alejandro Guirao <lekumberri@gmail.com>
2016-02-22 08:31:04 +0100

    Refactor package example

--- chapter_8/src/using_package/main.go
+++ chapter_8/src/using_package/main.go
@@ -1,10 +1,10 @@
 package main
 
 import "fmt"
-import "math"
+import "alex/math"
 
 func main() {
-	xs := []float64{1, 2, 3, 4}
+	xs := []float64{1, 2, 3, 4, 5}
 	avg := math.Average(xs)
 	fmt.Println(avg)
 }
